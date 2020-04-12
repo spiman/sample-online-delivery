182767ae66b5eca654f85ba747eb60593eac7ccc
Spyros Mandekis <spyros.mandekis@market.com>
2020-04-12 15:41:57 +0300

    whitelist env overrides in config

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,11 @@ import * as nconf from 'nconf';
 const file = (process.env.CONFIG_FILE || 'config.json');
 
 nconf.use('memory')
-    .env({ lowerCase: true, separator: '__' })
+    .env({
+        lowerCase: true,
+        separator: '__',
+        whitelist: ['mongo:uri', 'fixer:key']
+    })
     .file(file)
 
 export default nconf;
